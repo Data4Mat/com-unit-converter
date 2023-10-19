499f48ce95d0ebd47600e20e5800207a0695cd0e
ChrisJohDev <chris@chrisjohannesson.com>
2023-10-19 10:59:31 +0200

    Added selectors for chosing temperature or length conversion and implemented logic. Added styling and installed @lnu/eslint-config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,49 @@ import {
   Imperial1DUnits,
   TemperatureUnits,
 } from "d4m-unit-converter"
+import { create } from 'domain'
 
 export default function Home() {
   const unitconverter = new UnitConverter()
   const metricToImperial = unitconverter.MetricToImperial
   const imperialToMetric = unitconverter.ImperialToMetric
   const temperature = unitconverter.TemperatureConverter
+  const [activeField, setActiveField] = useState(true)
+
+  const loadTemperature = () => {
+    const options1 = createOptions(TemperatureUnits)
+    const options2 = createOptions(TemperatureUnits)
+    const select1 = document.querySelector<HTMLSelectElement>("#unit1")
+    const select2 = document.querySelector<HTMLSelectElement>("#unit2")
+    const label1 = document.querySelector<HTMLLabelElement>("#measure1Label")
+    const label2 = document.querySelector<HTMLLabelElement>("#measure2Label")
+    select1!.innerHTML = options1
+    select2!.innerHTML = options2
+    label1!.textContent = "Temperature"
+    label2!.textContent = "Temperature"
+  }
+
+  const loadLength = () => {
+    const options1 = createOptions(Metric1DUnits)
+    const options2 = createOptions(Imperial1DUnits)
+    const select1 = document.querySelector<HTMLSelectElement>("#unit1")
+    const select2 = document.querySelector<HTMLSelectElement>("#unit2")
+    const label1 = document.querySelector<HTMLLabelElement>("#measure1Label")
+    const label2 = document.querySelector<HTMLLabelElement>("#measure2Label")
+    select1!.innerHTML = options1
+    select2!.innerHTML = options2
+    label1!.textContent = "Metric"
+    label2!.textContent = "Imperial"
+  }
+
+  const createOptions = (units: any) => {
+    let options = ""
+    for (let key in units) {
+      options += `<option value="${key}">${units[key]}</option>`
+    }
+
+    return options
+  }
 
   const convertMetricToImperial = (e: React.MouseEvent<HTMLInputElement>) => {
     e.preventDefault()
@@ -31,22 +68,22 @@ export default function Home() {
     document.querySelector<HTMLInputElement>("#imperial1")!.value = resultRounded.toString()
   }
 
-  const convertImperialToMetric = (e: React.MouseEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    const imperialInput = document.querySelector<HTMLInputElement>("#imperial2")
-    const imperialLength = imperialInput?.value ? Number(imperialInput.value) : 0
-    const metricSelect = document.querySelector<HTMLSelectElement>("#metricType2")
-    const metricType = Metric1DUnits[metricSelect?.value as keyof typeof Metric1DUnits]
-    const imperialSelect = document.querySelector<HTMLSelectElement>("#imperialType2")
-    const imperialType = Imperial1DUnits[imperialSelect?.value as keyof typeof Imperial1DUnits]
+  // const convertImperialToMetric = (e: React.MouseEvent<HTMLInputElement>) => {
+  //   e.preventDefault()
+  //   const imperialLength = imperialInput?.value ? Number(imperialInput.value) : 0
+  //   const imperialInput = document.querySelector<HTMLInputElement>("#imperial2")
+  //   const metricSelect = document.querySelector<HTMLSelectElement>("#metricType2")k
+  //   const metricType = Metric1DUnits[metricSelect?.value as keyof typeof Metric1DUnits]
+  //   const imperialSelect = document.querySelector<HTMLSelectElement>("#imperialType2")
+  //   const imperialType = Imperial1DUnits[imperialSelect?.value as keyof typeof Imperial1DUnits]
 
-    const precision = imperialInput?.value?.split(".")[1]?.length || 2
+  //   const precision = imperialInput?.value?.split(".")[1]?.length || 2
 
-    const result = imperialToMetric.convert(imperialLength, imperialType, metricType)
-    const resultRounded = Math.floor(result * Math.pow(10, precision) + 0.5) / Math.pow(10, precision)
+  //   const result = imperialToMetric.convert(imperialLength, imperialType, metricType)
+  //   const resultRounded = Math.floor(result * Math.pow(10, precision) + 0.5) / Math.pow(10, precision)
 
-    document.querySelector<HTMLInputElement>("#metric2")!.value = resultRounded.toString()
-  }
+  //   document.querySelector<HTMLInputElement>("#metric2")!.value = resultRounded.toString()
+  // }
 
   const convertTemperatures = (e: React.MouseEvent<HTMLInputElement>) => {
     e.preventDefault()
@@ -65,91 +102,77 @@ export default function Home() {
     document.querySelector<HTMLInputElement>("#toTemperature")!.value = resultRounded.toString()
   }
 
+  const setFocusField = (e: React.FocusEvent<HTMLInputElement>) => {
+    if (e.target.id === "measure1") {
+      setActiveField(true)
+    } else {
+      setActiveField(false)
+    }
+  }
+
+  function conversionHandler(ev: ChangeEvent<HTMLInputElement>): void {
+    console.log("\n*** conversionHandler target.value: " + ev.target.value)
+    if (ev.target.value === 'temperature') {
+      loadTemperature()
+    } else if (ev.target.value === 'length') {
+      loadLength()
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative flex place-items-center">
         <div className="flex flex-col items-center justify-top z-[1]">
           <h1 className="mb-4">Unit Converter App</h1>
           <div className="mt-4 p-4 border-2 rounded-md">
-            <h2 className="mb-4 text-center">Chose Conversion from</h2>
-            <div className="flex flex-row justify-center">
+            {/* <h2 className="mb-4 text-center">Chose Conversion from</h2> */}
+            <div className="flex flex-col items-center">
+              <form className="flex flex-col items-center">
+                <fieldset className='flex flex-col items-center m-2 border p-3'>
+                  <legend className='text-lg'>Conversion:</legend>
+                  <div className="flex flex-col">
+                    <label htmlFor="temperature">
+                      <input type="radio"
+                        name="conversion"
+                        id="temperature"
+                        value='temperature'
+                        className="text-black mr-2 p-1"
+                        onChange={conversionHandler} />Temperature</label>
+                    <label htmlFor="length">
+                      <input type="radio"
+                        name="conversion"
+                        id="length"
+                        className="text-black mr-2 p-1"
+                        value='length'
+                        onChange={conversionHandler} /> Length</label>
+                  </div>
+                </fieldset>
+              </form>
               <form className="flex flex-col">
                 <h2 className="text-center mb-4"> Metric to Imperial</h2>
                 <div className="flex flex-row">
                   <div className="flex flex-col">
-                    <input title="Imperial length input" type="text" name="metric" id="metric1" className="text-black mt-2 p-1" />
-                    <select title="Metric Unit" name="metricType" id="metricType1" className="text-black mt-2 p-1" >
-                      <option value="kilometer">Kilometers</option>
-                      <option value="meter">Meter</option>
-                      <option value="decimeter">Decimeter</option>
-                      <option value="centimeter">Centimeter</option>
+                    <label id="measure1Label" htmlFor="measure1">Metric</label>
+                    <input title="Numeric Only" type="text"
+                      name="measure" id="measure1" className="text-black mt-2 p-1"
+                      pattern="\d*(\.\d+)?$" readOnly={!activeField}
+                      onFocus={setFocusField} />
+                    <select title="Selector Unit 1" name="unit-selector" id="unit1" className="text-black mt-2 p-1" >
                     </select>
                   </div>
                   <div className="flex flex-col ml-2">
-                    <input title="Imperial length input" type="text" name="imperial" id="imperial1" className="text-black mt-2 p-1" readOnly />
-                    <select title="Metric Unit" name="metricType" id="imperialType1" className="text-black mt-2 p-1">
-                      <option value="mile">Mile</option>
-                      <option value="yard">Yard</option>
-                      <option value="foot">Foot</option>
-                      <option value="inch">Inch</option>
-                      <option value="mil">Mil</option>
+                  <label id="measure2Label" htmlFor="measure1">Metric</label>
+                    <input title="Numeric Only" type="text"
+                      name="measure" id="measure2" className="text-black mt-2 p-1"
+                      pattern="\d*(\.\d+)?$" readOnly={activeField}
+                      onFocus={setFocusField} />
+                    <select title="Selector Unit 2" name="unit-selector" id="unit2" className="text-black mt-2 p-1">
                     </select>
                   </div>
 
                 </div>
                 <div className=" flex flex-row justify-evenly mt-4">
                   <input type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" value="Convert" onClick={convertMetricToImperial} />
-                </div>
-              </form>
-              <form className="flex flex-col ml-4">
-                <h2 className="text-center mb-4">Imperial To Metric</h2>
-                <div className="flex flex-row">
-                  <div className="flex flex-col">
-                    <input title="Imperial length input" type="text" name="imperial" id="imperial2" className="text-black mt-2 p-1" />
-                    <select title="Metric Unit" name="metricType" id="imperialType2" className="text-black mt-2 p-1">
-                      <option value="mile">Mile</option>
-                      <option value="yard">Yard</option>
-                      <option value="foot">Foot</option>
-                      <option value="inch">Inch</option>
-                      <option value="mil">Mil</option>
-                    </select>
-                  </div>
-                  <div className="flex flex-col  ml-2">
-                    <input title="Imperial length input" type="text" name="metric" id="metric2" className="text-black mt-2 p-1" readOnly />
-                    <select title="Metric Unit" name="metricType" id="metricType2" className="text-black mt-2 p-1" >
-                      <option value="kilometer">Kilometers</option>
-                      <option value="meter">Meter</option>
-                      <option value="decimeter">Decimeter</option>
-                      <option value="centimeter">Centimeter</option>
-                    </select>
-                  </div>
-                </div>
-                <div className=" flex flex-row justify-evenly mt-4">
-                  <input type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" value="Convert" onClick={convertImperialToMetric} />
-                </div>
-              </form>
-              <form className="flex flex-col ml-4">
-                <h2 className="text-center mb-4">Temperatures</h2>
-                <div className="flex flex-row">
-                  <div className="flex flex-col">
-                    <input title="Imperial length input" type="text" name="tempFrom" id="fromTemperature" className="text-black mt-2 p-1" />
-                    <select title="Metric Unit" name="tempFromType" id="fromType" className="text-black mt-2 p-1">
-                      <option value="celsius">Celsius</option>
-                      <option value="fahrenheit">Fahrenheit</option>
-                      <option value="kelvin">Kelvin</option>
-                    </select>
-                  </div>
-                  <div className="flex flex-col  ml-2">
-                    <input title="Imperial length input" type="text" name="tempTo" id="toTemperature" className="text-black mt-2 p-1" readOnly />
-                    <select title="Metric Unit" name="tempToType" id="toType" defaultValue="fahrenheit" className="text-black mt-2 p-1" >
-                      <option value="celsius">Celsius</option>
-                      <option value="fahrenheit">Fahrenheit</option>
-                      <option value="kelvin">Kelvin</option>
-                    </select>
-                  </div>
-                </div>
-                <div className=" flex flex-row justify-evenly mt-4">
-                  <input type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" value="Convert" onClick={convertTemperatures} />
                 </div>
               </form>
             </div>
